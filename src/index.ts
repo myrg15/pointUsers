2a7b96ff217533a4953a0e04842c89973282d95c
myrg15 <merishield@gmail.com>
2023-10-23 22:05:43 +0200

    feat: database connection creation

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
 import express,{Request,Response} from "express";//cargamos e inicializamos el objeto express
 import { router as routerUsers } from "./routes/usersRoutes";
+import { AppDataSource } from "./routes/db";
 
 const app = express()//ejecuta la funcion y guardala en una constante
 
 const PORT = process.env.PORT || 3000
 
+//middleware
+app.use(express.json())
+
 app.use('/users', routerUsers)
 
-app.listen(PORT, ()=>{
-    console.log(`Server running ${PORT}`);
-})
+AppDataSource.initialize()
+    .then(() => {
+        console.log('Database connected');
+
+        app.listen(PORT, ()=>{
+            console.log(`Server running ${PORT}`)
+        })
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
+
+
+
 
 /*app.listen(3000, () =>console.log('Servidor levantado en 3000')); //instanciamos el servidor
 //app.get('/', (req:Request,res:Response)=>{
